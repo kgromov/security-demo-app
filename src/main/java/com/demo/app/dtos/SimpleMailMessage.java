e62bb43bb4edf2e2433bb4428f8ff93a824cea8f
Konstantin <konstantin@revimedia.com>
2022-07-23 23:40:31 +0300

    Test mailtrap

---
+++ src/main/java/com/demo/app/dtos/SimpleMailMessage.java
@@ -0,0 +1,18 @@
+package com.demo.app.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SimpleMailMessage {
+    private String id;
+    private String from;
+    private String to;
+    private String subject;
+    private String body;
+}
---
+++ src/main/java/com/demo/app/services/MailSenderService.java
@@ -0,0 +1,28 @@
+package com.demo.app.services;
+
+import com.demo.app.dtos.SimpleMailMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MailSenderService {
+    private final JavaMailSender mailSender;
+
+    public void sendMail(SimpleMailMessage mail) {
+        MimeMessagePreparator messagePreparator = mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            mimeMessage.setFrom(mail.getFrom());
+            messageHelper.setTo(mail.getTo());
+            messageHelper.setSubject(mail.getSubject());
+            messageHelper.setText(mail.getBody());
+        };
+        mailSender.send(messagePreparator);
+        log.info("Mail '{}' from {} to {} was successfully sent", mail.getSubject(), mail.getFrom(), mail.getTo());
+    }
+}
